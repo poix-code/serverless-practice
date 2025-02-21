91a35269c8bfc813aeaa87f52d9d525436c344c5
JUAN uribe <juribe1@directvla.com.ar>
2025-02-20 22:21:30 -0500

    Setting up the Email Service and Deploying it

--- aws-ses-email/handler.js
+++ aws-ses-email/handler.js
@@ -1,8 +1,50 @@
-exports.hello = async (event) => {
+"use strict";
+const AWS = require('aws-sdk');
+const ses = new AWS.SES();
+exports.hello = async (event, context) => {
+  console.log("Received:::", event);
+  const { to, from, subject, message } = JSON.parse(event.body);
+  
+  if (!to || !from || !subject || !message) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'Missing required fields',
+        input: event
+      }, null, 2)
+    };
+}
+
+const params = {
+  Destination: {
+    ToAddress: [to]
+  },
+  Message: {
+    Body: {
+      Text: { Data: message  }
+    },
+    Subject: { Data: subject },
+  },
+  Source: from
+};
+
+try {
+  await ses.sendEmail(params).promise();
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v4.0! Your function executed successfully!'
+      message: 'Email sent successfully',
+      success: true
     })
   };
+} catch (error) {
+  console.error(error);
+  return {
+    statusCode: 400,
+    body: JSON.stringify({
+      message: 'The email fail to send',
+      success: false
+    })
+  };
+}
 };
