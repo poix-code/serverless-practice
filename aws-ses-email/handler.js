0d97b3a6c12d99005ee1e3291f7a1fda8a09548b
JUAN uribe <juribe1@directvla.com.ar>
2025-02-23 12:07:59 -0500

    Added the handler and serverless files to run the lambda

--- aws-ses-email/handler.js
+++ aws-ses-email/handler.js
@@ -1,50 +1,61 @@
 "use strict";
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 const ses = new AWS.SES();
-exports.createContact = async (event, context) => {
+
+module.exports.createContact = async (event, context) => {
   console.log("Received:::", event);
   const { to, from, subject, message } = JSON.parse(event.body);
-  
+
   if (!to || !from || !subject || !message) {
     return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 400,
+      body: JSON.stringify({ message: " to or from... are not set properly!" }),
+    };
+  }
+  const params = {
+    Destination: {
+      ToAddresses: [to],
+    },
+    Message: {
+      Body: {
+        Text: { Data: message },
+      },
+      Subject: { Data: subject },
+    },
+    Source: from,
+  };
+  try {
+    await ses.sendEmail(params).promise();
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
+      statusCode: 200,
+      body: JSON.stringify({
+        message: "email sent successfully!",
+        success: true,
+      }),
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Origin": "*",
+      },
       statusCode: 400,
       body: JSON.stringify({
-        message: 'Missing required fields',
-        input: event
-      }, null, 2)
+        message: "The email failed to send",
+        success: true,
+      }),
     };
-}
-
-const params = {
-  Destination: {
-    ToAddresses: [to]
-  },
-  Message: {
-    Body: {
-      Text: { Data: message  }
-    },
-    Subject: { Data: subject },
-  },
-  Source: from
-};
-
-try {
-  await ses.sendEmail(params).promise();
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Email sent successfully',
-      success: true
-    })
-  };
-} catch (error) {
-  console.error(error);
-  return {
-    statusCode: 400,
-    body: JSON.stringify({
-      message: 'The email fail to send',
-      success: false
-    })
-  };
-}
+  }
 };
